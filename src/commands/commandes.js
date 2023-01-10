c383afb488702b7f0688ccb8a495b613365316da
BayDev <baydev.contact@gmail.com>
2023-01-10 18:35:22 +0100

    fix dev command not showing

--- src/commands/commandes.js
+++ src/commands/commandes.js
@@ -13,12 +13,15 @@ module.exports = {
     /** @param {ChatInputCommandInteraction} interaction */
     async execute(interaction) {
         const helpEmbed = new EmbedBuilder().setColor('Blurple').setTitle('Commandes disponibles');
+        const ownerId = interaction.client.application.owner instanceof User // Is the application managed by an individual or a team?
+            ? interaction.client.application.owner.id
+            : interaction.client.application.owner.ownerId;
 
         helpEmbed.data.description = '';
         for (const command of interaction.client.commands) { // Do this for each command loaded in the current instance
             /** @type {SlashCommandBuilder} */
             const builder = command[1].data;
-            if (builder.name === 'dev' && interaction.user.id !== interaction.client.application.owner.id) continue; // Show the "dev" command to the appplication manager only
+            if (builder.name === 'dev' && interaction.user.id !== ownerId) continue; // Show the "dev" command to the appplication manager only
             if (builder.name === 'ref' && interaction.user.id !== await Main.get('referee')) continue; // Show the "ref" command to the referee only
 
             const subCommands = builder.options.filter(o => o.options);
