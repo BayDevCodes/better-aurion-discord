9c6f3df40ea4384f2dc864d455f90cab5d7d18ee
BayDev <baydev.contact@gmail.com>
2023-01-22 00:57:40 +0100

    refresh averages

--- src/commands/dev.js
+++ src/commands/dev.js
@@ -1,7 +1,7 @@
 // Third-party module
 const { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder, User } = require('discord.js'); // Elements from the discord.js library
 
-const { Main } = require('../util/tables'); // Database table
+const { Main, Promotion } = require('../util/tables'); // Database table
 
 // Export the command's data & execute function
 module.exports = {
@@ -16,6 +16,10 @@ module.exports = {
                 .setDescription('L\'étudiant·e à promouvoir (s\'il·elle n\'apparaît pas dans la liste, utilise son ID).')
                 .setRequired(true)
             )
+        )
+        .addSubcommand(subcommand => subcommand
+            .setName('rafraîchir')
+            .setDescription('Recalculer les moyennes de chaque étudiant·e.')
         )
         .addSubcommand(subcommand => subcommand
             .setName('reléguer')
@@ -40,6 +44,18 @@ module.exports = {
                 .setDescription('*Cette commande est réservée au développeur*');
 
             return interaction.reply({ embeds: [restrictedEmbed], ephemeral: true });
+        }
+
+        if (interaction.options.getSubcommand() === 'rafraîchir') {
+            const refreshedEmbed = new EmbedBuilder()
+                .setTitle('Moyennes recalculées')
+                .setColor('Green')
+                .setDescription('*Les moyennes de chaque étudiant·e ont été recalculées*');
+            
+            interaction.reply({ embeds: [refreshedEmbed], ephemeral: true });
+            for (const student of await Promotion.all())
+                Promotion.set(`${student.id}.averages`, calculateAverages(await Promotion.get(`${student.id}.marks`))); // Update the averages object
+            return;
         }
 
         const user = interaction.options.getUser('utilisateur');
